7e0eae07fd8fe05d18be343bf4022802773b1188
kathy_chell <chela.lara76@gmail.com>
2024-11-01 18:33:57 -0600

    Agregar archivos de proyecto.

---
+++ EjercicioPolimorfismo/Animal.cs
@@ -0,0 +1,14 @@
+﻿
+namespace EjercicioPolimorfismo
+{
+    public abstract class Animal
+    {
+        public abstract void Mover();
+
+        // Método virtual que puede ser sobrescrito en las clases derivadas
+        public virtual void Hablar()
+        {
+            Console.WriteLine("El animal hace un sonido.");
+        }
+    }
+}
